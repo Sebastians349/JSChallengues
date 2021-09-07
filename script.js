797e9c79b8c8c7b3114a1086f0ad3575c975ff79
Sebastian Simarro <ssebastian210@gmail.com>
2021-09-07 19:23:19 -0300

    push commit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-// Coding Challenge #1
+// Coding Challenge #1 CHECK
 // Mark and John are trying to compare their BMI (Body Mass Index), which is
 // calculated using the formula:
 // Your tasks:
@@ -29,7 +29,7 @@ let markHigherBMI = markBmi > johnBmi;
 // Test data:
 // § Data 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76
 // m tall.
-//              BREAKPOINT
+//              BREAKPOINT CHECK
 // Coding Challenge #2
 // Use the BMI example from Challenge #1, and the code you already wrote, and
 // improve it.
@@ -45,13 +45,13 @@ let markHigherBMI = markBmi > johnBmi;
 //  else{ console.log(`John's BMI (${johnBmi}) is higher than Mark's! BMI (${markBmi})`);
 // }
 
-// BREAKPOINT
+// CODING CHALLENGE 3 BREAKPOINT CHECK
 
 // Test data:
 // § Data 1: Dolphins score 96, 108 and 89. Koalas score 88, 91 and 110
 
-const averageDolphins = (96 + 108 + 89) / 3;
-const averageKoalas = (88 + 91 + 110) / 3;
+// const averageDolphins = (96 + 108 + 89) / 3;
+// const averageKoalas = (88 + 91 + 110) / 3;
 
 // § Data Bonus 1: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 123
 // const averageDolphins = (97+112+101) /3;
@@ -88,29 +88,81 @@ const averageKoalas = (88 + 91 + 110) / 3;
 // both teams have the same score and both have a score greater or equal 100
 // points. Otherwise, no team wins the trophy
 
-// BREAKPOINT
+// BREAKPOINT CHECK
 
 ////////////////////////////////////
 // Coding Challenge #4
 
 // Steven wants to build a very simple tip calculator for whenever he goes eating in a
-// restaurant. In his country, it's usual to tip 15% if the bill value is between 50 and
-// 300. If the value is different, the tip is 20%.
+// restaurant. In his country, it's usual to tip 15% if the bill value is between 50 and  300. If the value is different, the tip is 20%.
 // Your tasks:
 // 1. Calculate the tip, depending on the bill value. Create a variable called 'tip' for
 // this. It's not allowed to use an if/else statement � (If it's easier for you, you can
 // start with an if/else statement, and then try to convert it to a ternary
-// operator!)
-// 2. Print a string to the console containing the bill value, the tip, and the final value
-// (bill + tip). Example: “The bill was 275, the tip was 41.25, and the total value
-// 316.25”
 // Test data:
 // § Data 1: Test for bill values 275, 40 and 430
 // Hints:
 // § To calculate 20% of a value, simply multiply it by 20/100 = 0.2
 // § Value X is between 50 and 300, if it's >= 50 && <= 300 �
+// operator!)
+
+// const billValue = 275;
+// const tip =
+//   billValue >= 50 && billValue <= 300 ? billValue * 0.15 : billValue * 0.2;
+// // 2. Print a string to the console containing the bill value, the tip, and the final value
+// // (bill + tip). Example: “The bill was 275, the tip was 41.25, and the total value
+// // 316.25”
+// console.log(
+//   `The bill was ${billValue}, the tip was ${tip}, and the total value ${
+//     billValue + tip
+//   }`
+// );
+// COMO FUNCTION CHECK
+// const tip = (v) =>
+//   v >= 50 && v <= 300 ? console.log(v * 1.15) : console.log(v * 1.2);
+// tip(100);
 
 // BREAKPOINT
+// Coding Challenge #1
+
+// Back to the two gymnastics teams, the Dolphins and the Koalas! There is a new gymnastics discipline, which works differently.
+// Each team competes 3 times, and then the average of the 3 scores is calculated (so one average score per team).
+// A team ONLY wins if it has at least DOUBLE the average score of the other team. Otherwise, no team wins!
+
+// 1. Create an arrow function 'calcAverage' to calculate the average of 3 scores
+const calcAverage = (a, b, c) => (a + b + c) / 3;
+
+// 2. Use the function to calculate the average for both teams
+let avgDolphins = calcAverage(44, 23, 71);
+let avgKoalas = calcAverage(65, 54, 49);
+// TEST DATA 1: Dolphins score 44, 23 and 71. Koalas score 65, 54 and 49
+// TEST DATA 2: Dolphins score 85, 54 and 41. Koalas score 23, 34 and 27
+
+// 3. Create a function 'checkWinner' that takes the average score of each team as parameters ('avgDolphins' and 'avgKoalas'), and then logs the winner to the console, together with the victory points, according to the rule above. Example: "Koalas win (30 vs. 13)".
+
+/* FIX
+const checkWinner = (avgDolphins, avgKoalas) => {
+  avgDolphins > avgKoalas || avgDolphins >= 2 * avgKoalas
+    ? console.log(`Dolphins win (${avgDolphins} vs. ${avgKoalas})`)
+    : console.log(`Koalas win (${avgKoalas} vs. ${avgDolphins})`);
+}; */
+// 4. Use the 'checkWinner' function to determine the winner for both DATA 1 and DATA 2.
+// 5. Ignore draws this time.
+
+const checkWinner = function (avgDolphins, avgKoalas) {
+  if (avgDolphins >= 2 * avgKoalas) {
+    console.log(`Dolphins win 🏆 (${avgDolphins} vs. ${avgKoalas})`);
+  } else if (avgKoalas >= 2 * avgDolphins) {
+    console.log(`Koalas win 🏆 (${avgKoalas} vs. ${avgDolphins})`);
+  } else {
+    console.log('No team wins...');
+  }
+};
+// checkWinner(scoreDolphins, scoreKoalas);
+checkWinner(576, 111);
+
+// HINT: To calculate average of 3 values, add them all together and divide by 3
+// HINT: To check if number A is at least double number B, check for A >= 2 * B. Apply this to the team's average scores 😉
 
 // BREAKPOINT
 // BREAKPOINT
@@ -120,7 +172,6 @@ const averageKoalas = (88 + 91 + 110) / 3;
 // BREAKPOINT
 // BREAKPOINT
 // BREAKPOINT
-
 // BREAKPOINT
 ///////////////////////////////////////
 // Coding Challenge #1
@@ -155,3 +206,19 @@ BONUS TEST DATA 2: [1, 5, 3, 9, 6, 1]
 
 GOOD LUCK 😀
 */
+// BREAKPOINT
+// Coding Challenge #2
+// This is more of a thinking challenge than a coding challenge 🤓
+// Your tasks:
+// 1. Take the IIFE below and at the end of the function, attach an event listener that
+// changes the color of the selected h1 element ('header') to blue, each time
+// the body element is clicked. Do not select the h1 element again!
+// 2. And now explain to yourself (or someone around you) why this worked! Take all
+// the time you need. Think about when exactly the callback function is executed,
+// and what that means for the variables involved in this example.
+// (function () {
+//   const header = document.querySelector('h1');
+//   header.style.color = 'red';
+//   document.querySelector('body').addEventListener('click');
+// })();
+// GOOD LUCK 😀
