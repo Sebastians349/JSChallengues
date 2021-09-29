1d80bccf26d9a2744e7c2913b05eb348679bfaf6
Sebastian Simarro <ssebastian210@gmail.com>
2021-09-29 15:49:25 -0300

    push

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 // Coding Challenge #1 CHECK
 // Mark and John are trying to compare their BMI (Body Mass Index), which is
@@ -122,7 +122,7 @@ let markHigherBMI = markBmi > johnBmi;
 //   v >= 50 && v <= 300 ? console.log(v * 1.15) : console.log(v * 1.2);
 // tip(100);
 
-// BREAKPOINT
+// BREAKPOINT CHECK
 // Coding Challenge #1
 
 // Back to the two gymnastics teams, the Dolphins and the Koalas! There is a new gymnastics discipline, which works differently.
@@ -155,16 +155,80 @@ const checkWinner = function (avgDolphins, avgKoalas) {
   } else if (avgKoalas >= 2 * avgDolphins) {
     console.log(`Koalas win 🏆 (${avgKoalas} vs. ${avgDolphins})`);
   } else {
-    console.log('No team wins...');
+    console.log("No team wins...");
   }
 };
 // checkWinner(scoreDolphins, scoreKoalas);
-checkWinner(576, 111);
+// checkWinner(576, 111);
 
 // HINT: To calculate average of 3 values, add them all together and divide by 3
 // HINT: To check if number A is at least double number B, check for A >= 2 * B. Apply this to the team's average scores 😉
+// BREAKPOINT CHECK
+
+///////////////////////////////////////
+// Coding Challenge #2
+
+// Steven is still building his tip calculator, using the same rules as before: Tip 15% of the bill if the bill value is between 50 and 300, and if the value is different, the tip is 20%.
+
+// 1. Write a function 'calcTip' that takes any bill value as an input and returns the corresponding tip, calculated based on the rules above (you can check out the code from first tip calculator challenge if you need to). Use the function type you like the most. Test the function using a bill value of 100.
+const calcTip = function (bill) {
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+};
+// console.log(calcTip(301));
+// 2. And now let's use arrays! So create an array 'bills' containing the test data below.
+// TEST DATA: 125, 555 and 44
+const bills = [125, 555, 44];
+// 3. Create an array 'tips' containing the tip value for each bill, calculated from the function you created before.
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+// 4. BONUS: Create an array 'total' containing the total values, so the bill + tip.
+const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+// console.table(totals);
+
+// HINT: Remember that an array needs a value in each position, and that value can actually be the returned value of a function! So you can just call a function as array values (so don't store the tip values in separate variables first, but right in the new array) 😉
+
+// GOOD LUCK 😀
 
 // BREAKPOINT
+///////////////////////////////////////
+// Coding Challenge #3
+// Let's go back to Mark and John comparing their BMIs! This time, let's use objects to implement the calculations! Remember: BMI = mass / height ** 2 = mass / (height * height). (mass in kg and height in meter)
+
+// 1. For each of them, create an object with properties for their full name, mass, and height (Mark Miller and John Smith)
+
+// 2. Create a 'calcBMI' method on each object to calculate the BMI (the same method on both objects). Store the BMI value to a property, and also return it from the method.
+const mark = {
+  fullName: "Mark Miller",
+  mass: 78,
+  height: 1.69,
+  calcBmi: function () {
+    this.bmi = this.mass / this.height ** 2;
+    return this.bmi;
+  },
+};
+const john = {
+  fullName: "John Smith",
+  mass: 92,
+  height: 1.95,
+};
+
+mark.calcBmi();
+console.log(mark.bmi);
+// const mark = {
+//   fullName: 'Mark Miller',
+//   mass: 78,
+//   height: 1.69,
+//   calcBMI: function () {
+//     this.bmi = this.mass / this.height ** 2;
+//     return this.bmi;
+//   }
+// };
+
+// 3. Log to the console who has the higher BMI, together with the full name and the respective BMI. Example: "John Smith's BMI (28.3) is higher than Mark Miller's (23.9)!"
+
+// TEST DATA: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m tall.
+
+// GOOD LUCK 😀
+
 // BREAKPOINT
 // BREAKPOINT
 // BREAKPOINT
